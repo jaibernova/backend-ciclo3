f29e1fe8fb5503218d77af1e9acb6f0b547fdc26
jdnovar <jdnovar@unal.edu.co>
2021-11-17 15:05:04 -0500

    Database

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //URL A LA BASE DE DATOS
 //const URI = 'mongodb+srv://jaiber:nintendo64@freecluster.2o9li.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const URL = 'mongodb+srv://root:root@ferremax0.cal9t.mongodb.net/Ferremax0?retryWrites=true&w=majority';
+const URL = 'mongodb+srv://root:ferremax2021@ferremax.miudj.mongodb.net/Ferremax?retryWrites=true&w=majority';
 
 //MENSAJE DE CONEXION EXITOSA
 mongoose.connect(URL)
